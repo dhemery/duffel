e7dd53e92d8082e218f6ca2c617295769a9073f8
Dale Emery <dale@dhemery.com>
2025-05-20 16:33:10 -0700

    Change install code to use FS instead of os calls

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/fs"
 	"os"
 	"path/filepath"
 )
@@ -11,45 +12,96 @@ func main() {
 	run(os.Args[1:])
 }
 
+type duffelFS interface {
+	fs.ReadDirFS
+	Lstat(path string) (fs.FileInfo, error)
+	MkdirAll(path string, perm fs.FileMode) error
+	Symlink(old, new string) error
+}
+
+type request struct {
+	fsys   duffelFS
+	source string
+	target string
+	pkgs   []string
+}
+
+type duffelDirFS struct {
+	fs.ReadDirFS
+	Dir string
+}
+
+func NewDuffelFS(dir string) *duffelDirFS {
+	return &duffelDirFS{
+		ReadDirFS: os.DirFS(dir).(fs.ReadDirFS),
+		Dir:       dir,
+	}
+}
+
+func (f *duffelDirFS) Lstat(path string) (fs.FileInfo, error) {
+	return os.Lstat(filepath.Join(f.Dir, path))
+}
+
+func (f *duffelDirFS) MkdirAll(path string, perm fs.FileMode) error {
+	return os.MkdirAll(filepath.Join(f.Dir, path), perm)
+}
+
+func (f *duffelDirFS) Symlink(oldname, newname string) error {
+	return os.Symlink(oldname, filepath.Join(f.Dir, newname))
+}
+
 func run(args []string) error {
 	flags := flag.NewFlagSet("duffel", flag.ExitOnError)
 	source := flags.String("source", ".", "The source `dir`")
 	target := flags.String("target", "..", "The target `dir`")
 
 	flags.Parse(args)
+	root := "/"
 
-	targetDir, err := filepath.Abs(*target)
+	fsys := NewDuffelFS(root)
+
+	absTarget, err := filepath.Abs(*target)
 	if err != nil {
 		return fmt.Errorf("making target absolute: %w", err)
 	}
+	targetPath, _ := filepath.Rel(root, absTarget)
 
-	sourceDir, err := filepath.Abs(*source)
+	absSource, err := filepath.Abs(*source)
 	if err != nil {
 		return fmt.Errorf("making source absolute: %w", err)
 	}
+	sourcePath, _ := filepath.Rel(root, absSource)
 
-	sourceLinkDest, err := filepath.Rel(targetDir, sourceDir)
+	req := request{
+		fsys:   fsys,
+		source: sourcePath,
+		target: targetPath,
+		pkgs:   flags.Args(),
+	}
+	return install(req)
+}
+
+func install(r request) error {
+	sourceLinkDest, err := filepath.Rel(r.target, r.source)
 	if err != nil {
 		return fmt.Errorf("making source link dest: %w", err)
 	}
 
-	pkgs := flags.Args()
-
-	for _, pkg := range pkgs {
-		pkgDir := filepath.Join(sourceDir, pkg)
+	for _, pkg := range r.pkgs {
+		pkgDir := filepath.Join(r.source, pkg)
 		pkgLinkDest := filepath.Join(sourceLinkDest, pkg)
-		entries, err := os.ReadDir(pkgDir)
+		entries, err := r.fsys.ReadDir(pkgDir)
 		if err != nil {
 			return fmt.Errorf("reading entries of %q: %w", pkgDir, err)
 		}
 		for _, e := range entries {
-			linkPath := filepath.Join(targetDir, e.Name())
+			linkPath := filepath.Join(r.target, e.Name())
 			linkDest := filepath.Join(pkgLinkDest, e.Name())
-			err := os.MkdirAll(filepath.Dir(linkPath), 0o755)
+			err := r.fsys.MkdirAll(filepath.Dir(linkPath), 0o755)
 			if err != nil {
 				return fmt.Errorf("making link parent: %w", err)
 			}
-			err = os.Symlink(linkDest, linkPath)
+			err = r.fsys.Symlink(linkDest, linkPath)
 			if err != nil {
 				return fmt.Errorf("making link: %w", err)
 			}
