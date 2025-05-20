25015268a4e99ce69a624b623b88ce296304cc96
Dale Emery <dale@dhemery.com>
2025-05-19 18:36:01 -0700

    Add more (failing) dir opt tests

--- duffel_test.go
+++ duffel_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"io/fs"
 	"os"
 	"path/filepath"
@@ -9,24 +8,41 @@ import (
 )
 
 type dirOptTest struct {
-	dirs           []string
+	pkgItem        string
 	wd             string
-	sourceOpt      string
-	targetOpt      string
-	pkgs           []string
+	args           []string
 	wantTargetPath string
 	wantTargetDest string
 }
 
 var dirOptTests = map[string]dirOptTest{
-	"empty source and target options": {
-		dirs:           []string{"root/home/user/source/pkg/pkgItem"},
-		wd:             "root/home/user/source",
-		sourceOpt:      "", // Defaults to .: root/home/user/source
-		targetOpt:      "", // Defaults to ..: root/home/user
-		pkgs:           []string{"pkg"},
-		wantTargetPath: "root/home/user/pkgItem",
+	"Default source and target": {
+		wd:             "home/user/source",
+		pkgItem:        "home/user/source/pkg/pkgItem",
+		args:           []string{"pkg"},
+		wantTargetPath: "home/user/pkgItem",
 		wantTargetDest: "source/pkg/pkgItem",
+	},
+	"Default target, given source": {
+		wd:             "home/user/target/wd",
+		pkgItem:        "home/user/source/pkg/pkgItem",
+		args:           []string{"-source", "../source", "pkg"},
+		wantTargetPath: "home/user/target/pkgItem",
+		wantTargetDest: "../source/pkg/pkgItem",
+	},
+	"Default source, given target": {
+		wd:             "home/user/source",
+		pkgItem:        "home/user/source/pkg/pkgItem",
+		args:           []string{"-target", "../target", "pkg"},
+		wantTargetPath: "home/user/target/pkgItem",
+		wantTargetDest: "../source/pkg/pkgItem",
+	},
+	"Given source and target": {
+		wd:             "home/user/wd",
+		pkgItem:        "home/user/source/pkg/pkgItem",
+		args:           []string{"-source", "../source", "-target", "../target", "pkg"},
+		wantTargetPath: "home/user/target/pkgItem",
+		wantTargetDest: "../source/pkg/pkgItem",
 	},
 }
 
@@ -34,38 +50,47 @@ func TestDirOptions(t *testing.T) {
 	for name, test := range dirOptTests {
 		t.Run(name, func(t *testing.T) {
 			tmpDir := t.TempDir()
-			for _, dir := range test.dirs {
-				err := os.MkdirAll(filepath.Join(tmpDir, dir), 0o755)
-				if err != nil {
-					t.Fatal("setup: making dir:", err)
-				}
-			}
-
 			wd := filepath.Join(tmpDir, test.wd)
+
+			mustMkDir(t, filepath.Join(tmpDir, test.pkgItem))
+			mustMkDir(t, wd)
+
 			t.Chdir(wd)
 
-			run(test.pkgs)
+			run(test.args)
 
 			wantTargetPath := filepath.Join(tmpDir, test.wantTargetPath)
-			installedEntry, err := os.Lstat(wantTargetPath)
-			if err != nil {
-				t.Fatal("installed entry:", err)
-			}
-
-			wantType := fs.ModeSymlink
-			gotType := installedEntry.Mode() & fs.ModeType
-			if gotType != wantType {
-				t.Fatalf("got installed file type %O, want %O", gotType, wantType)
-			}
-
-			gotDest, err := os.Readlink(wantTargetPath)
-			if err != nil {
-				t.Fatal("installed link dest:", err)
-			}
+			gotDest := mustReadLink(t, wantTargetPath)
 
 			if gotDest != test.wantTargetDest {
-				t.Errorf("want link dest %q, got %q", test.wantTargetDest, gotDest)
+				t.Errorf("want link dest %q, got %q\n", test.wantTargetDest, gotDest)
 			}
 		})
 	}
+}
+
+func mustMkDir(t *testing.T, dir string) {
+	t.Helper()
+	if err := os.MkdirAll(dir, 0o755); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func mustReadLink(t *testing.T, path string) string {
+	t.Helper()
+	entry, err := os.Lstat(path)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	gotType := entry.Mode() & fs.ModeType
+	if gotType != fs.ModeSymlink {
+		t.Fatalf("want a link (file type %O), got file %q is type %O\n", fs.ModeSymlink, path, gotType)
+	}
+
+	gotDest, err := os.Readlink(path)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return gotDest
 }
