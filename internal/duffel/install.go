b54ee398df584d91e7b13eb34dafe392eac7667f
Dale Emery <dale@dhemery.com>
2025-05-30 15:40:30 -0700

    Extract the WalkDirFunc to install a package

--- internal/duffel/install.go
+++ internal/duffel/install.go
@@ -15,18 +15,28 @@ type Installer struct {
 func (i *Installer) Plan(pkgs []string) error {
 	for _, pkg := range pkgs {
 		pkgDir := path.Join(i.Source, pkg)
-		err := fs.WalkDir(i.FS, pkgDir, func(dir string, d fs.DirEntry, err error) error {
-			if err != nil || dir == pkgDir {
-				return err
-			}
-			itemPath, _ := filepath.Rel(pkgDir, dir)
-			i.Planner.CreateLink(pkg, itemPath)
-
-			return nil
-		})
+		err := fs.WalkDir(i.FS, pkgDir, PlanInstallPackage(pkgDir, pkg, i.Planner))
 		if err != nil {
 			return err
 		}
 	}
 	return nil
+}
+
+func PlanInstallPackage(pkgDir string, pkg string, planner *Planner) fs.WalkDirFunc {
+	return func(dir string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		// Don't install the pkg dir itself
+		if dir == pkgDir {
+			return nil
+		}
+
+		itemPath, _ := filepath.Rel(pkgDir, dir)
+		planner.CreateLink(pkg, itemPath)
+
+		return nil
+	}
 }
